2ea67b5d14e4c57d542d0ca709dc22cd58336fe9
Vidhi Sharma <viddhiisharma@gmail.com>
2017-11-19 19:21:25 -0800

    First commit

---
+++ signup/app.js
@@ -0,0 +1,53 @@
+/**
+* Module dependencies.
+*/
+var express = require('express')
+  , routes = require('./routes')
+  , user = require('./routes/user')
+  , http = require('http')
+  , path = require('path');
+//var methodOverride = require('method-override');
+var app = express();
+var mysql      = require('mysql');
+var bodyParser=require("body-parser");
+var connection = mysql.createConnection({
+              host     : 'localhost',
+              user     : 'root',
+              password : '',
+              database : 'tests'
+            });
+ 
+connection.connect();
+ 
+global.db = connection;
+ 
+// all environments
+app.set('port', process.env.PORT || 8080);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', routes.index);//call for main index page
+app.get('/login', routes.index);//call for login page
+app.get('/signup', user.signup);//call for signup page
+
+app.post('/login', user.login);//call for login post
+app.post('/signup', user.signup);//call for signup post
+app.get('/home/dashboard', user.dashboard);//call for dashboard page after login
+//Middleware
+app.listen(8080)
+
+
+
+
+
+
+var session = require('express-session');
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { maxAge: 60000 }
+}))
+
