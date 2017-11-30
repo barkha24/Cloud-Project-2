64359be96ddcc718397bb036057188a39ca63bf2
Vidhi Sharma <viddhiisharma@gmail.com>
2017-11-30 01:22:51 -0800

    Add files via upload

---
+++ kinesis/create-table.py
@@ -0,0 +1,23 @@
+import boto3
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.create_table(
+    TableName='hashtags',
+    KeySchema=[
+        {
+            'AttributeName': 'hashtag',
+            'KeyType': 'HASH'
+        }
+    ],
+    AttributeDefinitions=[
+        {
+            'AttributeName': 'hashtag',
+            'AttributeType': 'S'
+        }
+    ],
+    # pricing determined by ProvisionedThroughput
+    ProvisionedThroughput={
+        'ReadCapacityUnits': 5,
+        'WriteCapacityUnits': 5
+    }
+)
+table.meta.client.get_waiter('table_exists').wait(TableName='hashtags')
