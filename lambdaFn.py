47c2b8ff0898c29341d5c22a2bbe25c38f086a57
barkha24 <31901676+barkha24@users.noreply.github.com>
2017-12-02 20:44:40 -0800

    lambda function
    lambda function to read json file from s3, convert it to text format and store it in another s3 bucket

---
+++ lambdaFn.py
@@ -0,0 +1,31 @@
+from __future__ import print_function
+from pprint import pprint
+import json 
+import boto3
+
+def lambda_handler(event, context):
+    client = boto3.client('s3') 
+    s3 = boto3.resource('s3', aws_access_key_id='AKIAJNEOSFR76VQBIVUA',aws_secret_access_key='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')
+    bucket = s3.Bucket('recipebucketv1')
+    bucket2 = s3.Bucket('txtmp3bucketv1')
+    
+    obj = client.get_object(Bucket='recipebucketv1', Key='chips_recipe.json')
+    
+    jdata=obj['Body'].read()
+    
+    parsed_json1 = json.loads(jdata)
+    recipeTitle=parsed_json1['recipeTitle']
+    recipeSleep=parsed_json1['recipeIns'][0]['sleep']
+    recipeIns=parsed_json1['recipeIns']
+    finalrecipe=''
+    
+    for ins in recipeIns:
+        outSleep=ins['sleep']
+        outIns=ins['instruction']
+        pprint('{}<break time = "{}">'.format(outIns, outSleep))
+        finalrecipe= finalrecipe + '{}<break time = "{}">'.format(outIns, outSleep)
+       
+    s3.Bucket('txtmp3bucketv1').put_object(Key='recipev1.txt', Body=finalrecipe)
+    
+    
+    return 'HiFi from Food Admin'  
