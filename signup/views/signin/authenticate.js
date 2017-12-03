79af835e07ef6c11149aa6ef5d1824407c9929a3
mojdehkeykhanzadeh <33049718+mojdehkeykhanzadeh@users.noreply.github.com>
2017-12-02 20:28:18 -0800

    Update authenticate.js

--- signup/views/signin/authenticate.js
+++ signup/views/signin/authenticate.js
@@ -29,8 +29,37 @@ $(document).ready(function() {
   } */    
   });
  });
-});
-~
+ $('#signup').submit(function(e) {
+    e.preventDefault();
+    $.ajax({
+    type: 'POST',
+    url: 'http://dock2.hyunwookshin.com:8084/signup',
+    data:{
+		    firstname:$("fname").val(),
+			lastnmae :$("lname").val(),
+            username: $("#username").val(),
+            password: $("#password").val()
+        },
+    success: function(resultdata){
+      $.ajax({
+          type: 'GET',
+          url: 'http://dock2.hyunwookshin.com:8084',
+          headers: {"x-access-token": resultdata.token},
+          success: function(newData){
+              console.log('success');
+              //console.log(newData);
+              window.location= 'http://dock2.hyunwookshin.com:8084'
+         }
+       });
+  },
+  complete: function () {
+        // Schedule the next request when the current one has been completed
 
-~
+        setTimeout(this.ajaxRequest, 4000);
+    }
+ /*  });
+  } */    
+  });
+ });
+});
 
