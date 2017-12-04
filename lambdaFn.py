de73ab653fefef134bf122cd2e46e2a8d5179045
barkha24 <31901676+barkha24@users.noreply.github.com>
2017-12-03 16:39:35 -0800

    updated lambda fn with s3 records and triggers for recipe json files addition

--- lambdaFn.py
+++ lambdaFn.py
@@ -6,11 +6,13 @@ import boto3
 
 def lambda_handler(event, context):
     client = boto3.client('s3') 
-    s3 = boto3.resource('s3', aws_access_key_id='****',aws_secret_access_key='***')
-    bucket = s3.Bucket('recipebucketv1')
+    s3 = boto3.resource('s3', aws_access_key_id='****',aws_secret_access_key='****')
+    #bucket = s3.Bucket('recipebucketv1')
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    key = event['Records'][0]['s3']['object']['key']
     bucket2 = s3.Bucket('txtmp3bucketv1')
     
-    obj = client.get_object(Bucket='recipebucketv1', Key='chips_r2.json')
+    obj = client.get_object(Bucket=bucket, Key=key)
     
     jdata=obj['Body'].read()
     
@@ -33,15 +35,15 @@ def lambda_handler(event, context):
     pprint(finalrecipe)
     pprint('****** Get ready for POLLY ******')
    
-    pollyclient = boto3.client('polly',aws_access_key_id='***', aws_secret_access_key='***')
-    
+    pollyclient = boto3.client('polly',aws_access_key_id='****', aws_secret_access_key='****')
+    mp3key=recipeTitle+'.mp3'
     response = pollyclient.synthesize_speech(
-    Text = '<speak>'+ finalrecipe +'</speak>',
+    Text = '<speak>'+'Here we go with'+ recipeTitle + finalrecipe +'</speak>',
     TextType ='ssml',
     VoiceId='Joanna',
     OutputFormat='mp3')
     with closing(response["AudioStream"]) as stream:
-                s3.Bucket('txtmp3bucketv1').put_object(Key='chips_r2.mp3', Body=stream.read())
+                s3.Bucket('txtmp3bucketv1').put_object(Key=mp3key, Body=stream.read())
     pprint(response)
 
     return 'HiFi from Food Admin'
